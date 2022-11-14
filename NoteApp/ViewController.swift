8e923a05af2ac660c636f507a5f8231aa38397b3
Nam Nguyễn <>
2022-11-14 10:09:04 +0700

    first commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Nam Nguyễn on 14/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
