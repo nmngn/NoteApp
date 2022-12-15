734a78743e0ded6b3227260be2c7b347c1a5e364
Nguyễn Trung Nam <>
2022-12-16 00:48:14 +0700

    biometric

--- NoteApp/Presentation/NoteContent/LockNote/LockNoteViewController.swift
+++ NoteApp/Presentation/NoteContent/LockNote/LockNoteViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import LocalAuthentication
 
 class LockNoteViewController: UIViewController {
 
@@ -15,6 +16,30 @@ class LockNoteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationButton()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        let context = LAContext()
+//        var error: NSError?
+//
+//        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+//            let reason = "Use Face ID or Touch ID to open this note"
+//
+//            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+//                [weak self] success, authenticationError in
+//
+//                DispatchQueue.main.async {
+//                    if success {
+//                        self?.unlockNote()
+//                    } else {
+// 
+//                    }
+//                }
+//            }
+//        } else {
+//            
+//        }
     }
     
     func getDataOfNote(id: String) {
@@ -31,19 +56,23 @@ class LockNoteViewController: UIViewController {
             print("Could not save. \(error), \(error.userInfo)")
         }
     }
+    
+    func unlockNote() {
+        if let dataNote = self.dataNote {
+            self.lockNote(id: dataNote.idNote, isLock: !dataNote.isLock)
+            self.getDataOfNote(id: dataNote.idNote)
+            if dataNote.isLock {
+                let vc = NoteContentViewController.init(nibName: NoteContentViewController.className, bundle: nil)
+                vc.dataContent = self.dataNote
+                self.title = ""
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+        }
+    }
 
     @IBAction func unlockAction(_ sender: UIButton) {
         self.presentAlertUnlock(actionAfterUnlockSuccess: { [weak self] in
-            if let dataNote = self?.dataNote {
-                self?.lockNote(id: dataNote.idNote, isLock: !dataNote.isLock)
-                self?.getDataOfNote(id: dataNote.idNote)
-                if dataNote.isLock {
-                    let vc = NoteContentViewController.init(nibName: NoteContentViewController.className, bundle: nil)
-                    vc.dataContent = self?.dataNote
-                    self?.title = ""
-                    self?.navigationController?.pushViewController(vc, animated: true)
-                }
-            }
+            self?.unlockNote()
         })
     }
 }
